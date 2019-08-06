6c80f31e9f89cec7678639697b353925821b3eae
Samuel Fortune <s.fortune66@gmail.com>
2019-08-06 20:48:12 +1200

    Camera Zones & Positions encoded for Level Design 1

--- Prototype2/Assets/Scripts/CameraScript.cs
+++ Prototype2/Assets/Scripts/CameraScript.cs
@@ -32,6 +32,11 @@ public class CameraScript : MonoBehaviour
     void Awake()
     {
         zones = new Dictionary<int, BoxCollider>();
+        for (int i = 0; i < zoneManagerObject.transform.childCount; i++)
+        {
+            BoxCollider zoneI = zoneManagerObject.transform.GetChild(i).gameObject.GetComponent<BoxCollider>();
+            zones.Add(i, zoneI);
+        }
         playerIsInZone = new Dictionary<int, bool>();
         for (int i = 0; i < zones.Count; i++)
         {
@@ -44,7 +49,26 @@ public class CameraScript : MonoBehaviour
             { 2, new Vector3(-4.9F, 1.0F, -6.0F) },
             { 3, new Vector3(-5.8F, 1.2F, -4.0F) },
             { 4, new Vector3(-4.7F, 1.2F, -2.5F) },
-            { 5, new Vector3(-4.7F, 1.0F, -1.5F) }
+            { 5, new Vector3(-4.7F, 1.0F, -1.5F) },
+            { 6, new Vector3(-3.3F, 1.0F, -0.35F) },
+            { 7, new Vector3(-4.0F, 1.1F, -3.2F) },
+            { 8, new Vector3(-1.0F, 1.5F, -3.2F) },
+            { 9, new Vector3(-0.2F, 0.8F, -2.4F) },
+            { 10, new Vector3(-0.2F, 1.7F, -2.8F) },
+            { 11, new Vector3( 2.45F, 1.7F, -2.4F) },
+            { 12, new Vector3( 4.98F, 0.92F, -2.57F) },
+            { 13, new Vector3( 4.68F, 0.92F, -2.57F) },
+            { 14, new Vector3( 4.28F, 2.27F, -2.68F) },
+            { 15, new Vector3( 2.96F, 1.27F, -3.0F) },
+            { 16, new Vector3( 3.28F, 1.55F, 0.3F) },
+            { 17, new Vector3(-3.8F, 2.6F, -3.0F) },
+            { 18, new Vector3(-3.66F, 1.79F, -4.08F) },
+            { 19, new Vector3(-1.69F, 1.84F, -2.38F) },
+            { 20, new Vector3(4.87F, 1.52F, -2.91F) },
+            { 21, new Vector3(-5.16F, 1.74F, -1.44F) },
+            { 22, new Vector3(-4.75F, 1.67F, -4.07F) },
+            { 23, new Vector3(-0.35F, 0.69F, -3.07F) },
+            { 24, new Vector3(-5.97F, 1.66F, -4.05F) }
         };
     }
 
